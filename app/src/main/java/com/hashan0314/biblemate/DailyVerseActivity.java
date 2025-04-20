f9186a8db26a3fcf5ad0288f0cb3792b4f99ede6
hashan.silva <hashan.silva@gmail.com>
2025-04-20 21:33:39 +0200

    HA:Bible_MATE:change the view

--- app/src/main/java/com/hashan0314/biblemate/DailyVerseActivity.java
+++ app/src/main/java/com/hashan0314/biblemate/DailyVerseActivity.java
@@ -4,6 +4,8 @@ import static java.time.LocalDate.now;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,34 +21,68 @@ import org.openapitools.client.model.DayResponse;
 import org.openapitools.client.model.WeekResponse;
 
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class DailyVerseActivity extends AppCompatActivity {
 
     private static final String TAG = "BibleVerseActivity";
 
+    private TextView textDate, textSeason, textSundayCycle, textWeekdayCycle, textLothVolume, textRosarySeries;
+    private DatePicker datePicker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_daily_verse);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        datePicker = findViewById(R.id.datePicker);
+        textDate = findViewById(R.id.textDate);
+        textSeason = findViewById(R.id.textSeason);
+        textSundayCycle = findViewById(R.id.textSundayCycle);
+        textWeekdayCycle = findViewById(R.id.textWeekdayCycle);
+        textLothVolume = findViewById(R.id.textLothVolume);
+        textRosarySeries = findViewById(R.id.textRosarySeries);
+
+        datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                String selectedDate = String.format(Locale.getDefault(), "%04d-%02d-%02d", year, monthOfYear + 1, dayOfMonth);
+                fetchBibleVerse(LocalDate.parse(selectedDate));
+            }
         });
+
+        fetchBibleVerse(getSelectedDate());
+    }
+
+    private LocalDate getSelectedDate() {
+        int year = datePicker.getYear();
+        int month = datePicker.getMonth() + 1;
+        int day = datePicker.getDayOfMonth();
+        String selectedDate = String.format(Locale.getDefault(), "%04d-%02d-%02d", year, month, day);
+        return LocalDate.parse(selectedDate);
+    }
+
+    private void fetchBibleVerse(LocalDate selectedDate) {
         new Thread(() -> {
             ApiClient defaultClient = Configuration.getDefaultApiClient();
             defaultClient.setBasePath("https://liturgy.day/api");
 
             DefaultApi apiInstance = new DefaultApi(defaultClient);
-            LocalDate date = now();
 
             try {
-                DayResponse result = apiInstance.day(date);
-                Log.d(TAG, "Bible Verse: " + result.toString());
-                // Optionally update UI via runOnUiThread()
+                DayResponse result = apiInstance.day(selectedDate);
+
+                runOnUiThread(() -> {
+                    textDate.setText(result.getDate().toString());
+                    textSeason.setText(result.getSeason().getValue());
+                    textSundayCycle.setText(result.getSundayCycle().getValue());
+                    textWeekdayCycle.setText(result.getWeekdayCycle().getValue());
+                    textLothVolume.setText(result.getLothVolume().getValue());
+                    textRosarySeries.setText(result.getRosarySeries().getValue());
+                });
+
             } catch (ApiException e) {
-                Log.e(TAG, "Exception when calling DefaultApi#day", e);
+                Log.e(TAG, "API Error", e);
             }
         }).start();
     }
